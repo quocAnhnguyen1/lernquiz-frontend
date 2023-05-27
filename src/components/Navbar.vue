c7458bbbb6405f1888a082219c4de4ba04d95e89
HoerigDominic <s0578252@htw-berlin.de>
2023-05-27 16:49:20 +0200

    M3 Vue Projekt

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">CardBrain</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="navbar-nav">
+                    <router-link class="nav-link" to="/">Home</router-link>
+                    <router-link class="nav-link" to="/about">About</router-link>
+                    <router-link class="nav-link" to="/login">Login</router-link>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Nav-bar"
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>This is an login page</h1>
+</template>
+
+<script>
+export default {
+    name: "Log-in"
+}
+</script>
+
+<style scoped>
+
+</style>
